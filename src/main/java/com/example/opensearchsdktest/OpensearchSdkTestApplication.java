128f85586430d28f19dbbf6c683ce432a63a03e6
feistel <170395263+jk-feistel@users.noreply.github.com>
2024-10-02 16:44:56 +0900

    Create skeleton

---
+++ src/main/java/com/example/opensearchsdktest/OpensearchSdkTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.opensearchsdktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensearchSdkTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpensearchSdkTestApplication.class, args);
+    }
+
+}
